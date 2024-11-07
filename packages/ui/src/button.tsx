47afb2d24a9c617f1c882f55dff88c05d4836ca9
Philipp Strathausen <strathausen@gmail.com>
2024-11-07 21:41:21 +0100

    tweak button design to match

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -6,7 +6,7 @@ import { cva } from "class-variance-authority";
 import { cn } from ".";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -28,9 +28,9 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 shadow-none hover:underline",
       },
       size: {
-        sm: "h-8 rounded-md px-3 text-xs",
+        sm: "h-8 px-3 text-xs",
         md: "h-9 px-4 py-2",
-        lg: "h-10 rounded-lg px-8",
+        lg: "h-10 px-8",
         icon: "size-9",
       },
     },
