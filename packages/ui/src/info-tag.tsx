4ddee13b4bdd44e1923805c241d9019d83beef14
Philipp Strathausen <strathausen@gmail.com>
2024-11-13 00:03:04 +0100

    info tag format

--- packages/ui/src/info-tag.tsx
+++ packages/ui/src/info-tag.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 export function InfoTag({ icon, label }: Props) {
   return (
-    <span className="flex gap-2 bg-black px-2 py-1 text-sm text-white items-center">
+    <span className="flex items-center gap-2 bg-black px-2 py-1 text-sm text-white">
       {icon}
       {label}
     </span>
