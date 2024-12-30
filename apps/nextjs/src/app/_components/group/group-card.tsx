66caa15338178bc73d53503e9d38158434ece770
Philipp Strathausen <strathausen@gmail.com>
2024-12-30 10:48:12 +0100

    Enhance GroupCard layout: Add overflow handling and improve element positioning.

--- apps/nextjs/src/app/_components/group/group-card.tsx
+++ apps/nextjs/src/app/_components/group/group-card.tsx
@@ -11,7 +11,7 @@ export function GroupCard(group: RouterOutputs["group"]["search"][number]) {
   return (
     <Link href={`/group/${group.id}/meetups`}>
       <Box
-        className={`relative flex h-56 flex-col justify-between gap-2 ${group.status === "hidden" || group.status === "archived" ? "opacity-50" : ""}`}
+        className={`relative flex h-56 flex-col justify-between gap-2 overflow-hidden ${group.status === "hidden" || group.status === "archived" ? "opacity-50" : ""}`}
       >
         {group.image && (
           <Image
@@ -19,30 +19,35 @@ export function GroupCard(group: RouterOutputs["group"]["search"][number]) {
             alt={group.name}
             width={800}
             height={400}
-            className="absolute inset-0 object-fill"
+            className="absolute inset-0"
           />
         )}
-        <div className="z-10 line-clamp-2 flex flex-1 flex-col gap-2">
-          <h2 className="bg-white text-xl font-semibold uppercase">
+        <div className="z-10 flex flex-1 flex-col gap-2">
+          <h2 className="line-clamp-2 overflow-ellipsis bg-white text-xl font-semibold uppercase">
             {group.name}
           </h2>
           <p className="line-clamp-3 overflow-ellipsis bg-white pr-2">
             {group.description}
           </p>
         </div>
-        {group.nextMeetupDate && (
-          <p className="bg-white text-sm">
-            next event: {new Date(group.nextMeetupDate).toLocaleDateString()}
-          </p>
-        )}
-        <div className="flex justify-end gap-2">
-          {group.status === "archived" && (
-            <InfoTag label="archived" icon={<span>📦</span>} />
+        <div className="flex items-center justify-between z-10">
+          {group.nextMeetupDate && (
+            <p className="bg-white text-sm">
+              next event:{" "}
+              <strong>
+                {new Date(group.nextMeetupDate).toLocaleDateString()}
+              </strong>
+            </p>
           )}
-          {group.status === "hidden" && (
-            <InfoTag label="hidden" icon={<span>🕵️</span>} />
-          )}
-          <MembersCount count={group.membersCount} />
+          <div className="flex justify-end gap-2">
+            {group.status === "archived" && (
+              <InfoTag label="archived" icon={<span>📦</span>} />
+            )}
+            {group.status === "hidden" && (
+              <InfoTag label="hidden" icon={<span>🕵️</span>} />
+            )}
+            <MembersCount count={group.membersCount} />
+          </div>
         </div>
       </Box>
     </Link>
